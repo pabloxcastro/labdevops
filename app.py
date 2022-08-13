5c86cd0e178605519c0b7be1228348b48d41e1bf
pabloxcastro <pablo.castro@hotmail.com>
2022-08-13 12:45:45 -0300

    Custom message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Hello Word! Hola mundo! Bonjour le monde! Ciado mondo!"
 
 if __name__ == '__main__':
     app.run()
